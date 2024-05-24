5a89a885194a591925bb8d12666c4cbad40a3ad3
n6r10mnr <tomonorinomura.d@gmail.com>
2024-05-24 19:04:15 +0900

    meta変更

--- src/data/meta.ts
+++ src/data/meta.ts
@@ -1,7 +1,7 @@
-const meta =  {
+const meta = {
   front_page: {
-    title: "青春ループ - NTTドコモ",
-    description: "平成から令和へ、時代が変わっても変わらない高校生の恋する気持ち。青春真っ只中の人も、かつて青春を過ごした人も、きっと共感できる、それぞれの時代での想いの伝え方を描いた動画のスペシャルサイト。",
+    title: "title",
+    description: "description",
     keywords: "",
     og: {
       type: "website",
@@ -9,13 +9,13 @@ const meta =  {
       description: "",
       url: "",
       image: "",
-      site_name: "青春ループ - NTTドコモ"
+      site_name: "tofup",
     },
-    no_bot_crawl: false
+    no_bot_crawl: false,
   },
-  detail: {
-    title: "設定詳細｜青春ループ - NTTドコモ",
-    description: "動画中に隠された平成マッシュアップアイテムから、キャラクターの設定まで、主人公の女子高生2人のコメントとあわせて、「青春ループ」動画の裏側を紹介します。",
+  sample: {
+    title: "sample",
+    description: "description",
     keywords: "",
     og: {
       type: "article",
@@ -23,10 +23,10 @@ const meta =  {
       description: "",
       url: "",
       image: "",
-      site_name: "青春ループ - NTTドコモ"
+      site_name: "tofup",
     },
-    no_bot_crawl: false
+    no_bot_crawl: false,
   },
-}
+};
 
-module.exports = meta
+module.exports = meta;
