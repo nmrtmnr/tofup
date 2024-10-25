90416c7df89edd67ce41d572241c1435cc339c12
thakumi <kousakak12@gmail.com>
2024-10-25 19:15:24 +0900

    環境共有

--- _app/src/data/meta.ts
+++ _app/src/data/meta.ts
@@ -16,6 +16,23 @@ const meta = {
     touch_icon: "",
     no_bot_crawl: false,
   },
+  "/softbody/": {
+    title: "front_page",
+    description: "description",
+    keywords: "",
+    og: {
+      locale: "ja_JP",
+      url: "",
+      type: "website",
+      title: "",
+      description: "",
+      image: "",
+      site_name: "site name",
+    },
+    favicon: "",
+    touch_icon: "",
+    no_bot_crawl: false,
+  },
 };
 
 module.exports = meta;
